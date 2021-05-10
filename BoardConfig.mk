b25c701d8559d0b453008c5ebcac188e1c41645a
Alexandru Scurtu <sasha.scurtu@gmail.com>
2021-05-10 22:26:17 +0300

    [DNM] raphael: Ignore selinux neverallows
    * Fuck you, Dirac.
    
    Change-Id: If2df6adb12d28219c9541ed2ff7a0d9a6f54b745

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -215,6 +215,7 @@ VENDOR_SECURITY_PATCH := 2021-02-01
 USE_SENSOR_MULTI_HAL := true
 
 # Sepolicy
+SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 include device/xiaomi/raphael-sepolicy/raphael-sepolicy.mk
 
